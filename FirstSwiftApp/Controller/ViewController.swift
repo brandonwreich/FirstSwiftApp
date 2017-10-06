4c316db7cc34f68f841f92e92432a54daaa72e7f
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2017-10-06 09:34:45 -0600

    Made groupd

--- FirstSwiftApp/Controller/ViewController.swift
+++ FirstSwiftApp/Controller/ViewController.swift
@@ -37,7 +37,10 @@ class ViewController: UIViewController
         let redAmount = CGFloat (Double (arc4random_uniform(256))/255.00)
         let greenAmount = CGFloat (Double (arc4random_uniform(256))/255.00)
         let blueAmount = CGFloat (Double (arc4random_uniform(256))/255.00)
-        newColor = UIColor(red: redAmount, green: greenAmount, blue: blueAmount, alpha: CGFloat(1.0))
+        newColor = UIColor(red: redAmount,
+                           green: greenAmount,
+                           blue: blueAmount,
+                           alpha: CGFloat(1.0))
         return newColor
     }
     override func didReceiveMemoryWarning()
